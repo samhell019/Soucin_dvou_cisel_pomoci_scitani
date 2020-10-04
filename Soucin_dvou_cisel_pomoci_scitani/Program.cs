7162e9c1d7154fa7195670377ddabf425eab8148
Windows <Windows@DESKTOP-QCE28GP>
2020-10-04 14:45:18 +0200

    Přidejte soubory projektu.

---
+++ Soucin_dvou_cisel_pomoci_scitani/Program.cs
@@ -0,0 +1,12 @@
+﻿ště using System;
+
+namespace Soucin_dvou_cisel_pomoci_scitani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
